798d7f92ad6879c0d2093bc17d72b9b5c79d1cfa
sounabh <bagsounabh2003@gmail.com>
2026-02-27 10:31:02 +0530

    footerCommit

--- src/app/sections/Footer.jsx
+++ src/app/sections/Footer.jsx
@@ -1,7 +1,7 @@
 export default function AltuviaFooter() {
   return (
-    <footer className=" border-t border-gray-100">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8 ">
+    <footer className="bg-[#002147] border-t border-white/10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         
         {/* ========================== */}
         {/* Main Footer Grid Container */}
@@ -11,7 +11,7 @@ export default function AltuviaFooter() {
         {/* ========================== */}
         {/* Footer Bottom - Copyright */}
         {/* ========================== */}
-        <div className="border-t border-gray-200 pt-4 sm:pt-6 text-center text-sm text-gray-500 font-inter">
+        <div className="border-t border-white/10 pt-4 sm:pt-6 text-center text-sm text-white font-inter">
           © {new Date().getFullYear()} Altu<span className="text-[#3598FE]">via</span>. All rights reserved.
         </div>
       </div>
